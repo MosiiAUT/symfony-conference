89cbeff20b7d713b8f80eb538d075ae55b3bef25
amoser <andreas.moser@elements.at>
2022-06-15 10:25:39 +0200

    autowire per attribute (6.1)

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,14 +8,15 @@ use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class ConferenceController extends AbstractController
 {
     public function __construct(
+        #[Autowire("%kernel.project_dir%/public/uploads/photos")]
         private string $photoDir,
     ) {  
     }
